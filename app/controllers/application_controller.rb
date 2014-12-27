ab2102c4a375d3c34e8a3db8af97149cdb5cec82
Linnea <lkdamer@gmail.com>
2014-12-27 16:55:04 -0600

    made homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  private
+
+  def g_to_o grams
+    grams * 0.035274
+  end
+
+  def o_to_g ozs
+    ozs / 0.035274
+  end
 
 end
